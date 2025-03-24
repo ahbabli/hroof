2e47c7e51280da12c407f3ce628dca49e8ea3568
ahbabli <104667220+ahbabli@users.noreply.github.com>
2025-03-24 15:25:05 +0000

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,11 +4,11 @@ import { assets } from '../assets/assets'
 
 const Footer = () => {
   return (
-    <div className='flex justify-center flex-col items-center text-center font-mono mt-[300px] mb-10'>
+    <div className='flex justify-center flex-col items-center text-center font-mono md:mt-[300px] mb-10 mt-[600px]'>
         <p className='text-white'> للتواصل معي للمزيد من الأفكار أو دعمي بأي مبلغ رمزي</p>
         <div className='flex space-x-1'>
-        <a className='bg-white/75 border-2 text-blue-500 p-1 rounded cursor-pointer' href="paypal.me/ahbabli" target='_blank'><p >payPal</p></a>
-        <a className='bg-white/75 border-2 w-12 text-black p-1 rounded' href="X.com/ahbabli" target='_blank'><p>X</p></a>
+        <a className='bg-white/75 border-2 text-blue-500 p-1 rounded cursor-pointer' href="https://paypal.me/ahbabli" target='_blank'>payPal</a>
+        <a className='bg-white/75 border-2 w-12 text-black p-1 rounded' href="https://X.com/ahbabli" target='_blank'>X</a>
         </div>
 
     </div>
